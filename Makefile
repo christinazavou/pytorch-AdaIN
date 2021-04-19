cee6b420672c513d8fe87df09dfc07a8736ec9ed
christina <czavou01@gmail.com>
2021-04-19 08:54:09 +0300

    added batch size because neither cluster can handle 4

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 
 train-default:
 	export SAVE_DIR=/mnt/nfs/work1/kalo/maverkiou/zavou/data/adain_default/checkpoints \
-		&& export LOG_DIR=/mnt/nfs/work1/kalo/maverkiou/zavou/data/adain_default/checkpoints/logs \
+		&& export LOG_DIR=/mnt/nfs/work1/kalo/maverkiou/zavou/data/adain_default/logs \
 		&& export CONTENT_DIR=/mnt/nfs/work1/kalo/maverkiou/zavou/data/coco2017/train2017 \
 		&& export STYLE_DIR=/mnt/nfs/work1/kalo/maverkiou/zavou/data/wikiart/train \
 		&& export VGG_PATH=/mnt/nfs/work1/kalo/maverkiou/zavou/data/vgg_normalised.pth \
+		&& export BATCH_SIZE=4 \
 		&& export MAIN_FILE=train.py \
 		&& sbatch --job-name=adain --partition=titanx-long train.sh
--- train.sh
+++ train.sh
@@ -5,6 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --time=7-00:00         # Maximum runtime in D-HH:MM
+#SBATCH --mem-per-cpu=10000   # Memory in MB per cpu allocated
 
 
 SAVE_DIR=${SAVE_DIR:-coco_wikiart_ckpts}
@@ -12,16 +13,20 @@ LOG_DIR=${LOG_DIR:-coco_wikiart_logs}
 CONTENT_DIR=${CONTENT_DIR:-coco2017}
 STYLE_DIR=${STYLE_DIR:-wikiart}
 VGG_PATH=${VGG_PATH:-vgg_normalised.pth}
+BATCH_SIZE=${BATCH_SIZE:-8}
 MAIN_FILE=${MAIN_FILE:-train.py}
 echo "SAVE_DIR ${SAVE_DIR}"
 echo "LOG_DIR ${LOG_DIR}"
 echo "CONTENT_DIR ${CONTENT_DIR}"
 echo "STYLE_DIR ${STYLE_DIR}"
+echo "VGG_PATH ${VGG_PATH}"
+echo "BATCH_SIZE ${BATCH_SIZE}"
 echo "MAIN_FILE ${MAIN_FILE}"
 
 args="--content_dir ${CONTENT_DIR}"
 args="${args} --style_dir ${STYLE_DIR}"
 args="${args} --vgg ${VGG_PATH}"
+args="${args} --batch_size ${BATCH_SIZE}"
 args="${args} --save_dir ${SAVE_DIR}"
 args="${args} --log_dir ${LOG_DIR}"
 
